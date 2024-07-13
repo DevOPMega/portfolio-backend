d2425f34699dea212b45aabad83c700c6b0864bb
Adesh Singh <adeshsingh3002@gmail.com>
2024-07-14 01:03:12 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+require("dotenv").config();
+const router = require("./routes/index");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+const corsOptions = {
+    origin: true, //included origin as true
+    credentials: true, //included credentials as true
+};
+
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(router);
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT: ${PORT}`)
+})
