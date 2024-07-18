d80f7a56d0669bbe35763a3424c8b3484f094e84
Adesh Singh <adeshsingh3002@gmail.com>
2024-07-18 17:20:24 +0530

    route userinfo added

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const firebaseAuthController = require("../controllers/firebase-auth-controller");
+const FirebaseFirestoreController = require("../controllers/firebase-firestore-controller")
+const verifyToken = require("../middleware");
 
 router.post("/api/register", firebaseAuthController.registerUser);
 router.post("/api/login", firebaseAuthController.loginUser);
 router.post("/api/logout", firebaseAuthController.logoutUser);
 router.post("/api/reset-password", firebaseAuthController.resetPassword);
+router.get("/api/userinfo", verifyToken, FirebaseFirestoreController.getUserUsingUid);
 
 module.exports = router;
